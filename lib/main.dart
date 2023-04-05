43c30b9d8fb565dc7d211b693052b287af223b24
didier.cheung <didier.cheung@contentsquare.com>
2023-04-05 15:59:51 +0400

    feat display the counter of favorites

--- lib/main.dart
+++ lib/main.dart
@@ -167,23 +167,35 @@ class FavoritesPage extends StatelessWidget {
       );
     }
 
-    return GridView.count(
-      crossAxisCount: 2,
-      childAspectRatio: 300 / 80,
-      children: List.generate(appState.favorites.length, (index) {
-        return Padding(
-          padding: EdgeInsets.all(2.0),
-          child: ListTile(
-            leading: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                context.read<MyAppState>().onDelete(index);
-              },
-            ),
-            title: Text(appState.favorites[index].asLowerCase),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(30),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        Expanded(
+          child: GridView.count(
+            crossAxisCount: 2,
+            childAspectRatio: 300 / 80,
+            children: List.generate(appState.favorites.length, (index) {
+              return Padding(
+                padding: EdgeInsets.all(2.0),
+                child: ListTile(
+                  leading: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      context.read<MyAppState>().onDelete(index);
+                    },
+                  ),
+                  title: Text(appState.favorites[index].asLowerCase),
+                ),
+              );
+            }),
           ),
-        );
-      }),
+        ),
+      ],
     );
   }
 }
